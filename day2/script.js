dcff1ab0e5dff25d1e3f280f9b51df213dde5931
pranavraju1 <chatwithsoonu@gmail.com>
2026-05-25 16:38:05 +0530

    first push

---
+++ day2/script.js
@@ -0,0 +1,87 @@
+// console.log("hello")
+
+// let name = "pranav";
+
+// let age = 20;
+// let temp = age;
+// age = 22;       //re-initializing to update variable
+
+// console.log(age);      //22
+// console.log(temp);      //20
+
+/*
+asdasd
+asd
+asd
+asd
+*/
+
+
+// undefined -> it is an value assigned to a variable that is 
+// declared and not initialized 
+// let message;
+// console.log(message);        //undefined
+
+// let message;
+// message = "hello"
+// console.log(message)
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let a = 1, b = 2, c = 3;
+
+
+// varible naming rules in JS
+
+// 1. it can ouly start with a charecter(a-z) or $ or _ (also in between the name)
+// 2. try naming the variables in camel case
+
+
+// let myName = "pranav";
+// console.log(myName);
+
+
+
+// Data types
+
+// number
+// let age = 25;   //integer
+// let price = 25.55;  //decimal
+// let temp = -5;  //-ve value
+// let distance = 10000000000; //big int value
+// let value = 1.5e3; // 1500
+
+// console.log(age, price, temp, distance, value);
+
+// // string
+
+// let str1 = "hello1"; 
+// let str2 = 'hello2'; 
+
+// let myName = "pranav";
+
+// let str3 = `hello ${myName}`;
+
+// let str4 = "";  //emtpy string
+
+// console.log(str1, str2);    //hello1 hello2
+// console.log(str3);  //hello pranav 
+
+// Boolean
+// console.log(true)
+// console.log(false)
+
+// console.log(3 > 3)  //false
+// console.log(4 >= 3) //true
+// console.log(3 < 3)  //false
+// console.log(5 <= 3) //false
+
+// console.log(3 == '3')    //true
+// console.log(3 === '3')   //false
+// // Note: == only checks the value, === checks both value and datatype
+
+let age = 20;
+let canVote = age >= 18;
+console.log(canVote);       //true
---
+++ foundation/script.js
@@ -0,0 +1,20 @@
+// console.log("hello world 1")
+// console.log("hello world 2")
+
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+console.log("* * * * * * * * * * * * *")
+
+while (8 times) {
+
+        while (10 times) {
+           attach "*"   
+        }
+    console.log(print attached stars)
+}
