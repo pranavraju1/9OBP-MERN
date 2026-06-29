8d48807e9e8fd43f98b084f3e506438c5fdc5b68
Tarun Pandey <tpandey495@gmail.com>
2026-06-29 17:52:48 +0530

    linear search

---
+++ sorting/script.js
@@ -0,0 +1,23 @@
+// sorting algorithms:-  
+//inbuilt
+
+
+// sorting method in javascript sort the items of the array lexicographically.
+// let arr = ["apple","able","ball","boll","sun","son"];
+// arr.sort();
+// console.log(arr);
+
+// let arr = [101,202,1000,10];
+// arr.sort();
+// console.log(arr); // 10 1000 101 202
+
+
+// let arr = [200,300,202,2000]
+// arr.sort();
+// console.log(arr); //200 2000 202 300
+
+// custom sort 
+ let arr = [200,300,202,2000];
+ // asceding order
+sort(comparatorFunction);
+
