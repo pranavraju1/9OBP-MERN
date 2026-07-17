808c61382f9df6a98ee07faef2135e87f2ebc09e
Tarun Pandey <tpandey495@gmail.com>
2026-07-17 16:52:45 +0530

    nested array & string

--- module-2/2d-array/script.js
+++ module-2/2d-array/script.js
@@ -130,30 +130,261 @@
 // }
 
 
-let arr = [
-    [1, 2, 3, 4],
-    [4, 5, 6, 8],
-    [8, 9, 10, 11],
-    [8, 9, 15, 12]
-];
+// let arr = [
+//     [1, 2, 3, 4],
+//     [4, 5, 6, 8],
+//     [8, 9, 10, 11],
+//     [8, 9, 15, 12]
+// ];
 
 // task 1-> print sum row wise 
 // 10
 // 23
 // 38
 
-for(let row =0;row<arr.length;row++){
-    let sum =0;
-    for(let col =0;col<arr[row].length;col++){
-          sum+=arr[row][col];
-    }
-    console.log(sum);
-}
+// for(let row =0;row<arr.length;row++){
+//     let sum =0;
+//     for(let col =0;col<arr[row].length;col++){
+//           sum+=arr[row][col];
+//     }
+//     console.log(sum);
+// }
 
-
+// h.w
 //task 2-> print the matrix column 
 
 // task 2-> print sum column wise
 21
 
 // task 3-> search an item in the matrix 
+
+
+// 1st way
+// let arr = [ [1,2,3,4],
+//             [5,6,7,8],
+//             [9,10,11,12],
+//            [13,14,15,16]
+//         ];
+
+// 2nd way
+// let arr = [];
+
+// let mat = [[1,2,3,4],[4,5,6,7],[8,9,10,11]];
+
+// arr.push([1,2,3,4]);
+// arr.push([4,5,6,7]);
+// arr.push([8,9,10,11]);
+
+// console.log(arr);
+
+
+//sum of column wise
+
+// let mat = [[1, 2, 3, 4],
+// [4, 5, 6, 7],
+// [8, 9, 10, 11],
+// [12, 13, 14, 15]
+// ];
+
+// // 1 4 8 12-> 25
+// // 2 5 9 13-> 29
+// // 3 6 10 14 -> 33
+// // 4 7 11 15-> 37
+
+
+// for (let row = 0; row < mat.length; row++) {
+//     let sum = 0;
+//     for (let col = 0; col < mat[row].length; col++) {
+//         sum += mat[col][row]; // column wise sum
+//         console.log(mat[col][row]); // column wise traversal
+
+//     }
+//     console.log(sum);
+// }
+
+
+// // print  column wise 
+// let mat = [
+// [1, 2, 3, 4],
+// [4, 5, 6, 7],
+// [8, 9, 10, 11]
+// ];
+
+// for (let row = 0; row < mat[0].length; row++) {
+//     let sum = 0;
+//     for (let col = 0; col < mat.length; col++) {
+//         sum += mat[col][row]; // column wise sum
+//         console.log(mat[col][row]); // column wise traversal
+
+//     }
+//     console.log(sum);
+// }
+
+
+//matrix -> zig zag traversal
+// let mat = [
+// [1, 2, 3, 4],
+// [4, 5, 6, 7],
+// [8, 9, 10, 11]
+// ];
+
+
+// for (let row = 0; row < mat.length; row++) {
+//     let sum = 0;
+//     if(row%2==0){
+//          for (let col = 0; col < mat[row].length; col++) {
+//             console.log(mat[row][col]); 
+
+//         }
+//     }
+//     else{
+//         for (let col = mat[row].length-1; col >=0; col--) {
+//             console.log(mat[row][col]); 
+
+//         }
+//     }
+
+// }
+
+// find maximum element in the matrix
+// let mat = [
+// [1, 2, 3, 4],
+// [4, 5, 6, 7],
+// [8, 9, 10, 11]
+// ];
+
+// let max = -Infinity;
+
+// for(let row=0;row<mat.length;row++){
+//     for(let col =0;col<mat[0].length;col++){
+//         if(mat[row][col]>max){
+//             max=mat[row][col];
+//         }
+//     }
+// }
+
+// console.log(max);
+
+
+// find the target in the mat and print the row and column of it
+// let mat = [
+// [1, 2, 3, 4],
+// [4, 5, 6, 7],
+// [8, 9, 10, 11]
+// ];
+
+
+
+// let target = 5;
+// for(let row=0;row<mat.length;row++){
+//     for(let col =0;col<mat[0].length;col++){
+//         if(mat[row][col]==target){
+//             console.log(row,col);
+//         }
+//     }
+// }
+
+
+// spiral order traversal
+// let mat = [
+//     [1, 2, 3, 4, 5],
+//     [6, 7, 8, 9, 10],
+//     [11, 12, 13, 14, 15],
+//     [16, 17, 18, 19, 20],
+//     [21, 17, 18, 19, 22]
+// ]
+
+// let mat = [
+//     [1, 2, 3, 11],
+//     [4, 5, 6, 12],
+//     [7, 8, 9, 13]
+// ]
+
+// let top = 0, left = 0, right = mat[0].length - 1, bottom = mat.length - 1;
+// let ans = [];
+// while (left <= right && top <= bottom) {
+//     // left to right(top row)
+//     for (let i = left; i <= right; i++) {
+//         ans.push(mat[top][i]);
+//     }
+//     top++;
+//     // top to bottom 
+//     for (let i = top; i <= bottom; i++) {
+//         ans.push(mat[i][right]);
+//     }
+//     right--;
+//     // right to left
+//     for (let i = right; i >= left && top <= bottom; i--) {
+//         ans.push(mat[bottom][i]);
+//     }
+//     bottom--;
+//     // bottom to top
+//     for (let i = bottom; i >= top && left <= right; i--) {
+//         ans.push(mat[i][left]);
+//     }
+//     left++;
+// }
+
+// console.log(ans);
+
+// H.w 
+
+// q1
+
+// transpose of a matrix
+
+// Given a square matrix.find it's transpose
+// let mat = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+// transpose matrix
+// [
+
+
+// ]
+
+// q2: rotate matrix by 90 degree clockwise and anticlockwise 
+
+
+
+// substring and subarray
+
+// str = "abcdef";
+
+// substring/subarray -> continous part (cannot skip)
+// -> order Perserve 
+
+
+let arr = [1,4,5,6,7];
+// find subarrays of length 2 
+
+let k=2;
+for(let i=0;i<arr.length-k+1;i++){
+    let subArray=[];
+    for(let j=i;j<i+k;j++){
+        subArray.push(arr[j]);
+    }
+    console.log(subArray);
+}
+
+// [ 1, 4 ]
+// [ 4, 5 ]
+// [ 5, 6 ]
+// [ 6, 7 ]
+
+
+//let say using inbuild function is allowed(slice)
+
+
+//h.w
+// given a string find the substring of a given length k=2
+
+
+//h.w
+//given an array print all it's subarray
+
+
+// count all the substring of a given string 
