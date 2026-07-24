8da3f774adcaf6bf70e6d7bb3fb586ade5b6741a
Tarun Pandey <tpandey495@gmail.com>
2026-07-24 16:50:33 +0530

    revision 1

---
+++ module-2/revision/revision1.js
@@ -0,0 +1,124 @@
+// let n = 5 , m =4;
+// for(let row=0;row<n;row++){
+//     let str = "";
+//     for(let col =0;col<m;col++){
+//         if(row==0 || row == n-1 || col==0 || col == m-1){
+//             str+="*";
+//         }
+//         else{
+//             str+=" ";
+//         }
+//     }
+//     console.log(str);
+// }
+
+
+// let n = 5 , m =5;
+// for(let row=0;row<n;row++){
+//     let str = "";
+//     for(let col =0;col<m;col++){
+//         if(col==0 || row == col|| row==n-1){
+//             str+="*";
+//         }
+//         else{
+//             str+=" ";
+//         }
+//     }
+//     console.log(str);
+// }
+
+
+// Matrix Addition 
+
+// r1 == r2
+// c1 == c2
+
+
+// let mat1 = [
+//            [1,2,3],
+//            [4,5,6],
+//            [7,8,9]
+//            ]
+
+
+// let mat2 = [
+//            [9,10,3],
+//            [4,11,6],
+//            [7,8,9]
+//            ]
+
+// let res = [];
+
+// for(let row =0;row<mat1.length;row++){
+
+//     let temp = [];
+//     for(let col =0;col<mat1[row].length;col++){
+//          temp.push(mat1[row][col]+mat2[row][col]);
+//     }
+//     res.push(temp);
+// }
+
+// console.log(res);
+
+
+//  multiplication of a matrix -> h.w 
+
+
+
+
+// pefix and suffix sum
+
+// let arr = [1,2,3,4,5];
+
+// let preSum = []
+
+// for(let i=0;i<arr.length;i++){
+//     if(i==0){
+//         preSum[i]=arr[i];
+//     }
+//     else{
+//         preSum[i]=preSum[i-1]+arr[i];
+//     } 
+// }
+
+// console.log(preSum);
+
+// suffix sum ->h.w
+
+// let arr = [1,2,3,4,5];
+// let suffixSum = [15,14,12,9,5];
+
+
+// // find sum in a range 
+
+// let arr = [1,2,4,5,7,8];
+
+
+// sum(1,3)-> 11
+// sum(2,5) -> 24
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
