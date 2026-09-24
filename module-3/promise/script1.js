a52dce3e117fa5484ce6658d4e532ba9171ae650
Tarun Pandey <tpandey495@gmail.com>
2026-09-24 16:48:45 +0530

    promise

--- module-3/promise/script1.js
+++ module-3/promise/script1.js
@@ -1,15 +1,210 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    <script>
-        // const promise = new Promise((resolve, reject) => {
+// synchronous and Asynchronous 
+
+
+
+// setTimeout() -> excute a piece of code after specified time interval.
+// set time out returns a timerId 
+// clearTimeout(timerId)->
+
+// setInterval() -> keep excutating a piece of code after specified time interval.
+// clearInterval(id);
+
+
+// callback functionn-> function that is passed to another function as an argument.
+
+
+// function order(delivery){
+//     delivery();
+// }
+
+
+// function delivery(){
+//     console.log("item is dispatched for the delivery");
+// }
+
+// order(delivery);
+
+
+// approach 
+
+// function order(delivery){
+//     delivery();
+// }
+
+// order(function delivery(){
+//     console.log("item is dispatched for the delivery");
+// });
+
+
+// anonuymous function -> function that does'nt have any name.
+
+// let a=function(){
+//     console.log("this is anonymous fucntion")
+// }
+
+// a();
+
+
+// function order(delivery){
+//     delivery();
+// }
+
+// order(function(){
+//     console.log("item is dispatched for the delivery");
+// });
+
+
+// callback
+
+// function order(delivery){
+//     delivery();
+// }
+
+// order(()=>{
+//     console.log("item is dispatched for the delivery");
+// });
+
+
+// nested callback 
+// - multiple dependent asynchronous task
+//     - to handle mutliple handle asynchorous task
+//         - need nested callback
+
+// // pseudo code 
+// function login(cred, callback) {
+//     callback(error, user);
+// }
+
+
+// login(cred, (error, user) => {
+//     if (error)
+//         return;
+//     // console.log("user is logdin successfully")   
+
+//     getProfile(user.id, (error, profile) => {
+//         if (error)
+//             return;
+
+//         getCourse(user.id, (error, course) => {
+
+
+//             getAssignment(course.id, (error, assignment) => {
+
+
+//             })
+
+//         })
+//     })
+// })
+
+// strucutre -> 
+// pyramid 
+
+// pyramid of doom
+
+// cons of nested of callback 
+// - unreadable 
+// - unmaintaible 
+
+
+
+// one way to fix nesting is to use named function 
+// handleCourse(){
+
+// }
+
+// function handleProfile(error, profile) {
+//     if (error)
+//         return;
+//     getCourse(user.id, hanldeCourse)
+// }
+
+// function handleLogin(error, user) {
+//     if (error)
+//         return error;
+//     getProfile(userId, handleProfile);
+// }
+
+// login(cred, handleLogin);
+
+
+// not deeply nested 
+// jumping from one function to another 
+// tracing function can be harder 
+// still hard to maintain 
+
+
+// error first callback 
+// function handleLogin(error, user) {
+//     if (error)
+//         return error;
+// }
+
+// login(cred, handleLogin);
+
+// disadvantage :-
+// hard to maintain
+// pyramid of doom
+// bad readibility 
+// inversion of control 
+
+// promise -
+
+
+// use of prommise ->
+// solve the disadvantage of callback.
+// promised used to handle asynchronous operation.
+
+
+// asynchornous operation are
+// api call 
+// file reading 
+// database data fetch
+
+
+// resolve() -> called when asynchronous resolved.
+// reject() -> called when asynchronous operation rejected.
+
+// 1. promise is created using promise constructor 
+
+// const promise = new Promise((resolve, reject) => {
+//     //    resolve("Asynchornous task is resolved");
+//     reject("Promise rejected successfully");
+// })
+
+// promise consume 
+
+// promise.then((result)=>{
+//     console.log(result);
+// })
+
+
+// // promise catch 
+// promise.catch((error)=>{
+//    console.log(error);
+// })
+
+// State of the promise-> 
+// Pending 
+// Fullfilled 
+// Rejected 
+
+
+
+// Pending -> when a promise is created it's state is pending.
+
+const promise = new Promise((resolve, reject) => {
+    // //    resolve("Asynchornous task is resolved");
+    // reject("Promise rejected successfully");
+})
+
+console.log(promise);
+
+
+
+// const promise = new Promise((resolve, reject) => {
         //     // resolve("Asynchornous task is resolved");
         //     reject("Promise rejected successfully");
         // })
@@ -98,20 +293,3 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </script>
-</body>
-
-</html>
